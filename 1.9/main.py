bd225b4acdd2dfaf76def4b07661cd5c95f5e08e
Modifier20 <billyrobinpaisley@outlook.com>
2021-01-22 15:02:23 +0000

    1.9

---
+++ 1.9/main.py
@@ -0,0 +1,6 @@
+num01 = int(input("Number 1>>"))
+num02 = int(input("Number 2>>"))
+num03 = int(input("Number 3>>"))
+sum = num01 + num02 + num03
+answer = str(sum)
+print("\n" + "The Sum Of The Three Numbers Is \n>>" + answer)
