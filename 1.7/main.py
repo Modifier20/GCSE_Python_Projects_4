88f793a77a3406903ec1278c1e1f25d8721df102
Modifier20 <75219289+Modifier20@users.noreply.github.com>
2021-01-22 15:53:09 +0000

    Update main.py

--- 1.7/main.py
+++ 1.7/main.py
@@ -1,3 +1,6 @@
+# Created By: Modifier20
+# On: Fri 22nd January 2021
+
 num01 = input("Please Enter Your First Number: ")
 num02 = input("Please Enter Your Second Number: ")
 answer = num01 + num02
