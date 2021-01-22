73dc4140ce8bf0ce5e9c9a665cd24682b5ac6a56
Modifier20 <billyrobinpaisley@outlook.com>
2021-01-22 15:08:14 +0000

    1.10

---
+++ 1.10/main.py
@@ -0,0 +1,7 @@
+length = int(input("Length: "))
+width = int(input("Width: "))
+depth = int(input("Depth: "))
+volume = length * width * depth
+answer = str(volume)
+print("")
+print("The Volume of The Pool Is: " + answer)
