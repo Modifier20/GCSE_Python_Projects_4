dddac99420fbffe76f287bd31f92a9fbb4e3ea21
Modifier20 <billyrobinpaisley@outlook.com>
2021-01-22 15:17:14 +0000

    1.11

---
+++ 1.11/main.py
@@ -0,0 +1,9 @@
+num01 = int(input("Number 001>>"))
+num02 = int(input("Number 002>>"))
+div = num01 / num02
+rem = num01 % num02
+print("\nThe Answer Is>>", div, "\nWith a Remainder of>>", rem)
+
+
+
+
