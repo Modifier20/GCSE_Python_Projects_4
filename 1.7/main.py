2289887130610a5f09f193606bedd00593aad933
Modifier20 <billyrobinpaisley@outlook.com>
2021-01-22 14:50:24 +0000

    1.7

---
+++ 1.7/main.py
@@ -0,0 +1,5 @@
+num01 = input("Please Enter Your First Number: ")
+num02 = input("Please Enter Your Second Number: ")
+answer = num01 + num02
+print("")
+print(num01 + " + " + num02 + " = " + answer)
