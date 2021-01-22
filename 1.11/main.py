27cdc7ce3fc334400c5ee34e5244bdbf941dd5e2
Modifier20 <75219289+Modifier20@users.noreply.github.com>
2021-01-22 15:54:43 +0000

    1.11

--- 1.11/main.py
+++ 1.11/main.py
@@ -1,9 +1,8 @@
+# Created By: Modifier20
+# On: Fri 22nd January 2021
+
 num01 = int(input("Number 001>>"))
 num02 = int(input("Number 002>>"))
 div = num01 / num02
 rem = num01 % num02
 print("\nThe Answer Is>>", div, "\nWith a Remainder of>>", rem)
-
-
-
-
