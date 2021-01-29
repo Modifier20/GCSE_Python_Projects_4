72d5b476b21d4a56da00e23a028f96f3d36fb9ab
Modifier20 <billyrobinpaisley@outlook.com>
2021-01-29 13:21:37 +0000

    2.7

---
+++ 2.7/main.py
@@ -0,0 +1,15 @@
+temperature = int(input("Please Enter A Temperature: "))
+
+FREEZING = 0
+NORMAL = 1
+BOILING = 100
+
+if temperature <= 0:
+    print("Your Water Is Freezing!")
+elif temperature >= 1:
+    if temperature >= 100:
+        print("Your Water Is Boiling!")
+    else:
+        print("Your Water Is Normal!")
+else:
+    print("Invalid Character's Have Been Entered")
