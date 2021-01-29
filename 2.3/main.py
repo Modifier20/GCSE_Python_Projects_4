853836643a6394ca1f4d1e58f0bc9fc03e278b34
Modifier20 <billyrobinpaisley@outlook.com>
2021-01-29 12:47:36 +0000

    2.3

--- 2.3/main.py
+++ 2.3/main.py
@@ -5,17 +5,13 @@ num01 = int(input("Number 1>"))
 num02 = int(input("Number 2>"))
 num03 = int(input("Number 3>"))
 
-if num01 > num02 and num03:
-    print(num01, "is the largest number.")
-elif num02 > num01 and num03:
-    print(num02, "is the largest number.")
-elif num03 > num02 and num01:
-    print(num03, "is the largest number.")
-#elif num01 < num02 and num03:
-#    print("Worked")
-#elif num02 < num01 and num03:
-#    print("Worked")
-#elif num03 < num02 and num01:
-#    print("Worked")
+if num01 > num02:
+    if num01 > num03:
+        print("The Highest Number Was", num01)
+    else:
+        print("The Highest Number Was", num03)
 else:
-    print("Invalid Chars")
+    if num02 > num03:
+        print("The Highest Number Was", num02)
+    else:
+        print("The Highest Number Was", num03)
