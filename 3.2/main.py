33da5173a0b95763f8b93f7743bf4aa6364d6f99
Modifier20 <billyrobinpaisley@outlook.com>
2021-02-05 16:40:18 +0000

    3.2

---
+++ 3.2/main.py
@@ -0,0 +1,10 @@
+# Created By: Modifier20
+# On: Fri 5th Feb - 14:30
+
+message = str(input("Please Enter Your Message: "))
+repetition = int(input("Please Enter The Number of Times You Want it Repeated: "))
+
+counter = 0
+while counter < repetition:
+    print(message)
+    counter = counter + 1
