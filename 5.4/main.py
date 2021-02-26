96c1f76eebe5e548f58cc64dafd0fee05e9ba920
Modifier20 <billyrobinpaisley@outlook.com>
2021-02-26 15:29:04 +0000

    5.4

--- 5.4/main.py
+++ 5.4/main.py
@@ -26,18 +26,19 @@ StudentName = ["James", "Jill", "Jack", "Sophie", "Henry", "Angus"]
 DOB = ["02.05.1990", "20.12.2012", "01.01.1985", "08.08.1988", "28.02.2000", "05.02.2021"]
 
 print("STUDENT NAME(S):", StudentName)
-StudentDataRequest = input("Please Enter The Student's Position (0-5): ")
-if StudentDataRequest == "0":
+StudentDataRequest = input("Please Enter The Student's Name: ")
+
+if StudentName.index(StudentDataRequest) == 0:
     print("Born:", DOB[0])
-elif StudentDataRequest == "1":
+elif StudentName.index(StudentDataRequest) == 1:
     print("Born:", DOB[1])
-elif StudentDataRequest == "2":
+elif StudentName.index(StudentDataRequest) == 2:
     print("Born:", DOB[2])
-elif StudentDataRequest == "3":
+elif StudentName.index(StudentDataRequest) == 3:
     print("Born:", DOB[3])
-elif StudentDataRequest == "4":
+elif StudentName.index(StudentDataRequest) == 4:
     print("Born:", DOB[4])
-elif StudentDataRequest == "5":
+elif StudentName.index(StudentDataRequest) == 5:
     print("Born:", DOB[5])
 else:
     print("Invalid Input...")
