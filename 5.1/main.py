ab7cc312d1fbc989a564c8f50d3b96061e020211
Modifier20 <billyrobinpaisley@outlook.com>
2021-02-26 08:37:37 +0000

    5.1

---
+++ 5.1/main.py
@@ -0,0 +1,8 @@
+month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"]
+
+enteredNumber = int(input("Please Enter The Month Number (0-11): "))
+
+if enteredNumber != (1 <= 12):
+    print(month[enteredNumber])
+else:
+    print("You Haven't Entered A Valid Month Number.")
