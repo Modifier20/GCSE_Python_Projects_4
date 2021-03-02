299d56e17094dc717a470149a527f7328d4796d7
Modifier20 <billyrobinpaisley@outlook.com>
2021-03-02 15:44:05 +0000

    6.5

--- 6.5/main.py
+++ 6.5/main.py
@@ -1,24 +1,57 @@
 import random
-guessStatus = False
+attempts = 0
+ansStatus = "False"
 
-# RANDOMLY GENERATED NUMBERS.
-def computersGuess():
-    generatedGuess = random.randint(1,50)
-    return generatedGuess
+# THIS TAKES IN THE USERS RANGE INTO THE PROGRAM
+usersRange = int(input("Please Enter A Number For The Range> "))
 
-# GUESSING PROGRAM
-computersGuess()
-while guessStatus != True:
-    computersGuess()
+# THIS WILL GENERATE A RANDOM NUMBER FROM ZERO TO THE USERS RANGE
+def computerGeneratedGuess():
+    global computersGuess
+    global prevGuess
+    computersGuess = random.randint(0, usersRange)
+    prevGuess = computersGuess
+    return computersGuess + prevGuess
+# THIS GENERATES THE COMPUTERS HIGHER GUESS
+def higherComputerGeneratedGuess():
+    global computersGuess
+    global prevGuess
+    computersGuess = random.randint(prevGuess, usersRange)
+    prevGuess = computersGuess
+    return computersGuess + prevGuess
+# THIS GENERATES THE COMPUTERS LOWER GUESS
+def lowerComputerGeneratedGuess():
+    global computersGuess
+    global prevGuess
+    computersGuess = random.randint(0, prevGuess)
+    prevGuess = computersGuess
+    return computersGuess + prevGuess
+# THIS IS THE TRUE OF FALSE CODE
+def trueOrFalse():
+    global ansStatus
+    ansStatus = input("Did I Guess Correctly> ")
+    return ansStatus
 
+computerGeneratedGuess()
 
-
-    # THIS SECTION CHECKS TO SEE IF THE CORRECT GUESS IS TRUE OR FALSE
-    if guessStatus == False:
-        higherOrLower = input("Is it Higher or Lower> ")
-        if higherOrLower == "higher":
-            computersGuess()
-        elif higherOrLower == "Higher":
-            computersGuess()
-            
-    elif guessStatus == True:
+while ansStatus != "True":
+    print(computersGuess)
+    trueOrFalse()
+    #THIS PRINTS THE CONGRATULATIONS SENTENCE(S) AT THE END OF THE SCRIPT
+    if ansStatus == "True":
+        print("\nThat's Amazing, I Haven't Played This Before...\nGoodbye, and have a good day.")
+        print("It Took Me", attempts, "Attempts...")
+    else:
+        # THIS RUNS DIRECTS THE HIGHER OR LOWERE ENTRIES TO THE CORRECT FUNCTION AND ADD ONE TO THE ATTEMPTS SCORE.
+        attempts = attempts + 1
+        higherOrLower = input("Higher(H) or Lower(L) Then? ")
+        if higherOrLower == "H":
+            higherComputerGeneratedGuess()
+        elif higherOrLower == "h":
+            higherComputerGeneratedGuess()
+        elif higherOrLower == "L":
+            lowerComputerGeneratedGuess()
+        elif higherOrLower == "l":
+            lowerComputerGeneratedGuess()
+        else:
+            print("Invalid Input")
