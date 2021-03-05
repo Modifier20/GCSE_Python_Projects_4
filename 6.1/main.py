3d3625b7c3844f77da973ed487cab6f8bd70c9e6
Modifier20 <billyrobinpaisley@outlook.com>
2021-03-05 11:25:00 +0000

    7.2

--- 6.1/main.py
+++ 6.1/main.py
@@ -1,16 +1,21 @@
 def takenNumber():
     global largestValue
+
     numberOne = int(input("Please Enter Your First Number... "))
     numberTwo = int(input("Please Enter Your Second Number... "))
     numberThree = int(input("Please Enter Your Third Number... "))
+
     if numberOne > numberTwo and numberThree:
         largestValue = numberOne
     elif numberTwo > numberOne and numberThree:
         largestValue = numberTwo
-    elif numberThree > numberTwo and numberOne:
+    # THIS IS THE PROBLEMATIC CODE #####################
+    elif numberThree and numberTwo < numberThree:
         largestValue = numberThree
+    ####################################################
+
     else:
-        print("No Valid Answers Were Given")
+        print("Invalid Answer...")
     return largestValue
 
 takenNumber()
---
+++ 7.2/main.py
@@ -0,0 +1,22 @@
+usernamesAndPass = 'usersData.txt'
+with open(usernamesAndPass) as f_obj:
+    usernamesData = f_obj.read()
+
+# THIS IS THE USERS INPUT FOR THE USER NAME
+name = input("Enter a username: ")
+
+# THIS WILL DETERMINE WHETHER THE USERNAME IS FOUND IN THE USERS DATA FILE
+if name in usernamesData:
+    password = input("Please enter your password... ")
+
+    # THIS WILL DETERMINE WHETHER THE PASSWORD IS FOUND IN THE USERS DATA FILE
+    if password in usernamesData:
+        print("Welcome " + name + " please remember to log out of your session when you are done...")
+
+    # THIS IS THE ERROR MESSAGE FOR WHEN THE PASSWORD IS NOT RECOGNISED.
+    else:
+        print("We're very sorry but your password has not recognised in the system.\nYou may want to cosider ding an internet to find a solution.")
+
+# THIS IS THE ERROR MESSAGE FOR A FAILED USERNAME
+else:
+    print("We're very sorry but" + name + "is not recognised in the system.\nYou may want to cosider ding an internet to find a solution.")
