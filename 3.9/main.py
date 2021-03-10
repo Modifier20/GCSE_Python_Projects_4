db94fdb90e70bc5375824f523b81c3b9031bb8e9
Modifier20 <billyrobinpaisley@outlook.com>
2021-03-10 19:37:40 +0000

    3.9

---
+++ 3.9/main.py
@@ -0,0 +1,8 @@
+def countdown():
+    usersInteger = int(input("Please enter a number to count down from: "))
+    print(usersInteger)
+    while usersInteger > 0:
+        usersInteger = usersInteger - 1
+        print(usersInteger)
+
+countdown()
