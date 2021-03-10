a0b365893a0cbb2b1c72a6603e1161d8100a52f3
Modifier20 <billyrobinpaisley@outlook.com>
2021-03-10 20:39:28 +0000

    3.14

---
+++ 3.14/main.py
@@ -0,0 +1,9 @@
+zero = 0
+usersNumber = int(input("Please enter a number you want the primes up to: "))
+for number in range(zero, usersNumber + 1):
+   if number > 1:
+       for i in range(2, number):
+           if (number % i) == 0:
+               break
+       else:
+           print(number)
