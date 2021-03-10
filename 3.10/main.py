12bff16697e59283312d8e3c8cdaf872c620f77f
Modifier20 <billyrobinpaisley@outlook.com>
2021-03-10 19:52:57 +0000

    3.10

---
+++ 3.10/main.py
@@ -0,0 +1,52 @@
+
+# THIS TAKES IN THE USERS INPUT
+NthTermForMultiplication = int(input("Enter a number to multiply by: "))
+print()
+
+# ONE
+answer1 = 1 * NthTermForMultiplication
+print("1 *", NthTermForMultiplication, "=", answer1)
+
+# TWO
+answer2 = 2 * NthTermForMultiplication
+print("2 *", NthTermForMultiplication, "=", answer2)
+
+# THREE
+answer3 = 3 * NthTermForMultiplication
+print("3 *", NthTermForMultiplication, "=", answer3)
+
+# FOUR
+answer4 = 4 * NthTermForMultiplication
+print("4 *", NthTermForMultiplication, "=", answer4)
+
+# FIVE
+answer5 = 5 * NthTermForMultiplication
+print("5 *", NthTermForMultiplication, "=", answer5)
+
+# SIX
+answer6 = 6 * NthTermForMultiplication
+print("6 *", NthTermForMultiplication, "=", answer6)
+
+# SEVEN
+answer7 = 7 * NthTermForMultiplication
+print("7 *", NthTermForMultiplication, "=", answer7)
+
+# EIGHT
+answer8 = 8 * NthTermForMultiplication
+print("8 *", NthTermForMultiplication, "=", answer8)
+
+# NINE
+answer9 = 9 * NthTermForMultiplication
+print("9 *", NthTermForMultiplication, "=", answer9)
+
+# TEN
+answer10 = 10 * NthTermForMultiplication
+print("10 *", NthTermForMultiplication, "=", answer10)
+
+# ELEVEN
+answer11 = 11 * NthTermForMultiplication
+print("11 *", NthTermForMultiplication, "=", answer11)
+
+# TWELVE
+answer12 = 12 * NthTermForMultiplication
+print("12 *", NthTermForMultiplication, "=", answer12)
