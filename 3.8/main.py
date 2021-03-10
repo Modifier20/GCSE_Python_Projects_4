bc8d410367af23e1b9ef6ef1dc5e7e5b1e0b2c8d
Modifier20 <billyrobinpaisley@outlook.com>
2021-03-10 19:26:43 +0000

    3.8

---
+++ 3.8/main.py
@@ -0,0 +1,32 @@
+numbersArray = []
+
+integer01 = int(input("1) Please Enter Your First Number: "))
+numbersArray.append(integer01)
+
+integer02 = int(input("2) Please Enter Your First Number: "))
+numbersArray.append(integer02)
+
+integer03 = int(input("3) Please Enter Your First Number: "))
+numbersArray.append(integer03)
+
+integer04 = int(input("4) Please Enter Your First Number: "))
+numbersArray.append(integer04)
+
+integer05 = int(input("5) Please Enter Your First Number: "))
+numbersArray.append(integer05)
+
+integer06 = int(input("6) Please Enter Your First Number: "))
+numbersArray.append(integer06)
+
+integer07 = int(input("7) Please Enter Your First Number: "))
+numbersArray.append(integer07)
+
+integer08 = int(input("8) Please Enter Your First Number: "))
+numbersArray.append(integer08)
+
+print()
+print(numbersArray)
+print()
+
+sortedArray = sorted(numbersArray)
+print(sortedArray[7])
